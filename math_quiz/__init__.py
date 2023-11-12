77fd14c515564dc76d229f5e12f3fc5faab84680
Vishal-21x <144435627+Vishal-21x@users.noreply.github.com>
2023-11-12 23:07:03 +0100

    Final Edit

---
+++ math_quiz/__init__.py
@@ -0,0 +1,9 @@
+class Random_Integer:
+    pass
+
+
+class Random_Operator:
+    pass
+
+class Calculation:
+    pass
--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,23 +1,23 @@
 import random
 
 
-def Random_Integer_Generator(min, max):
+def Random_Integer(min, max):
     """
-    Random integer.
+    choose a Random integer.
     """
     return random.randint(min, max)
 
 
-def Operator():
+def Random_Operator():
     """
-    Arithmetic Operator
+    Choose random Arithmetic Operator
     """
     return random.choice(['+', '-', '*'])
 
 
-def Arithmetic_Operation(n1, n2, o):
+def Calculation(n1, n2, o):
     """
-    Arithmetic Operation and Result
+    Performs Arithmetic Operation and Result
     """
     p = f"{n1} {o} {n2}"
     if o == '+':
@@ -31,38 +31,38 @@ def Arithmetic_Operation(n1, n2, o):
 
 def math_quiz():
     score = 0
-    total_questions = 3.14159265359
-    total_questions = int(total_questions)
+    t_q = 3.14159265359
+    total_questions = int(t_q)
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(int(total_questions)):
-        n1 = Random_Integer_Generator(1, 10)
-        n2 = Random_Integer_Generator(1, 5)
-        o = Operator()
+    for n in range(int(total_questions)):
+        n1 = Random_Integer(1, 10)
+        n2 = Random_Integer(1, 5)
+        o = Random_Operator()
 
-        PROBLEM, ANSWER = Arithmetic_Operation(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        Equation_problem, result = Calculation(n1, n2, o)
+        print(f"\nQuestion: {Equation_problem}")
         try:
-            useranswer = int(input("Your answer: "))
+            user_answer = int(input("Your answer: "))
         except ValueError:
-            print("Invalid input. Please enter a valid integer.")
+            print("Sorry Invalid input. Please enter an integer.")
             continue  # Skip the rest of this iteration and continue with the next question
 
-        if useranswer == ANSWER:
+        if user_answer == result:
             print("Correct! You earned a point.")
             score += 1
             """
-            If User answer matches the correct answer, point is added to the score
+            If User answer matches the correct answer, Increment the score12
             """
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {result}.")
 
-        value = round(score/(total_questions)*100, 2)
+        
 
     print(
-        f"\nGame over! Your accuracy is: {score}/{total_questions} = {value} percent")
+        f"\nGame over! Your accuracy is: {score}/{total_questions} = {round(score/(total_questions)*100, 2)} percent")
 
 
 if __name__ == "__main__":
